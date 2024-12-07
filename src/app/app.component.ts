c28252eef2893c5a6d2dca52ff5ea015fa40c964
Anastasiia Zolotova <anastasya.zolotova11@gmail.com>
2024-12-07 16:21:03 +0200

    lab-1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DescriptionComponent } from './description/description.component';
+import { AboutComponent } from './about/about.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AboutComponent, DescriptionComponent], 
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
