880e08814fd0ca5e262c39cf31e60fd29de23dd8
Anastasiia Zolotova <anastasya.zolotova11@gmail.com>
2024-12-07 16:37:25 +0200

    lab-8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,20 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DescriptionComponent } from './description/description.component';
 import { AboutComponent } from './about/about.component';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+import { ThirdComponent } from './third/third.component';
+import { ParentComponent } from './parent/parent.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, AboutComponent, DescriptionComponent], 
+  imports: [RouterOutlet,
+    AboutComponent,
+    DescriptionComponent,
+    FirstComponent,
+    SecondComponent,
+    ThirdComponent,
+    ParentComponent], 
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
